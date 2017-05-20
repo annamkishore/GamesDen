62c084c1179ddb3daab102803707b857b0108c3b
Annam, Kishore <C-Kishore.Annam@charter.com>
2017-05-20 18:13:34 +0530

    static unlimited letters

--- src/gd/tap-me/tap-me.component.ts
+++ src/gd/tap-me/tap-me.component.ts
@@ -19,12 +19,15 @@ export class TapMeComponent implements OnInit, OnChanges {
   @Input()
   restartFlag: boolean;   // workaround flag to call restart method from HomeComponent
 
-  tiles: any;
+  tilesDataList: any;
   currTappedTileIndex: number;
   gameMessage: string;
 
   gameWatch: StopWatch;
   elapsedObj: Elapsed;
+
+  visibleLetterIndex: number;
+  lettersMap: any;
 
   constructor(private elRef: ElementRef, private utilService: UtilServiceService) {
   }
@@ -44,8 +47,8 @@ export class TapMeComponent implements OnInit, OnChanges {
       'english': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
         'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
       'numbers': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
-              '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40',
-              '41', '42', '43', '44', '45', '46', '47', '48', '49', '50']
+              '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32']
+              // , '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50']
     };
 
     if (this.mode === 'hindi') {
@@ -56,10 +59,12 @@ export class TapMeComponent implements OnInit, OnChanges {
     tappingModeMap[this.mode].map((item, index) => lettersMap.push({index, item}));
     const shuffleLength = lettersMap.length > this.gridSize ? this.gridSize : lettersMap.length;
     lettersMap = this.utilService.shuffle(lettersMap, shuffleLength);
-    this.tiles = [];
+    this.tilesDataList = [];
     for (let i = 0; i < shuffleLength; i++) {
-      this.tiles[i] = lettersMap[i];
+      this.tilesDataList[i] = lettersMap[i];
     }
+    this.visibleLetterIndex = shuffleLength;
+    this.lettersMap = lettersMap;
 
     this.gameMessage = null;
     this.currTappedTileIndex = -1;
@@ -69,16 +74,21 @@ export class TapMeComponent implements OnInit, OnChanges {
   }
 
   /**
-   * @param currIndex
+   * @param currLetterIndex
    */
-  onTap(currIndex) {
-    if (this.currTappedTileIndex + 1 === currIndex) {
-      // hide button
-      this.elRef.nativeElement.querySelectorAll('button[id=\'' + currIndex + '\']')[0].style.visibility = 'hidden';
+  onTap(currLetterIndex) {
+    if ((this.currTappedTileIndex + 1) % this.gridSize === currLetterIndex) {
+      if (this.visibleLetterIndex < this.lettersMap.length) {
+        // show new button
+        this.elRef.nativeElement.querySelector('button[id=\'' + currLetterIndex + '\'] h3').innerText = this.lettersMap[this.visibleLetterIndex].item;
+        this.visibleLetterIndex++;
+      }else {
+        // hide button
+        this.elRef.nativeElement.querySelectorAll('button[id=\'' + currLetterIndex + '\']')[0].style.visibility = 'hidden';
+      }
       this.currTappedTileIndex++;
-      console.log(this.currTappedTileIndex);
     }
-    if (this.currTappedTileIndex === this.tiles.length - 1) {
+    if (this.currTappedTileIndex === this.lettersMap.length - 1) {
       this.gameOver();
     }
   }
