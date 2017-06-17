c759bd776e957f51d86a1b66300cdf631dfb8471
Annam, Kishore <C-Kishore.Annam@charter.com>
2017-06-17 15:22:59 +0530

    Pen Style: Pattern added

--- src/gd/smart-slate/smart-slate.component.ts
+++ src/gd/smart-slate/smart-slate.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 
 enum PenStyleEnum {
   Basic,
-  Web
+  Web,
+  Pattern
 }
 
 @Component({
@@ -43,11 +44,11 @@ export class SmartSlateComponent implements OnInit {
     console.log('hello 33');
     this.canvasElement = <HTMLCanvasElement>document.getElementById('myCanvas');
 
-    this.canvasElement.addEventListener('touchstart', e => { this.mousePressed = true; this.draw(e, true); });
+    this.canvasElement.addEventListener('touchstart', e => this.onStartDraw(e));
     this.canvasElement.addEventListener('touchmove', e => { if (this.mousePressed) { this.draw(e, false); }});
     this.canvasElement.addEventListener('touchend', e => this.closeCurrPath());
 
-    this.canvasElement.addEventListener('mousedown', e => { this.mousePressed = true; this.draw(e, true); });
+    this.canvasElement.addEventListener('mousedown', e => this.onStartDraw(e));
     this.canvasElement.addEventListener('mousemove', e => { if (this.mousePressed) { this.draw(e, false); }});
     this.canvasElement.addEventListener('mouseup', e => this.closeCurrPath());
     this.canvasElement.addEventListener('mouseout', e => this.closeCurrPath());
@@ -57,16 +58,28 @@ export class SmartSlateComponent implements OnInit {
     this.penColor = '#FFFFFF';
     this.bgColor = '#000000';
     this.setBgColor();
-    this.penSize = 1;
-    this.penStyle = PenStyleEnum.Web;
+    this.penSize = 10;
+    this.penStyle = PenStyleEnum.Basic;
     this.points = new Array();
+  }
+
+  onStartDraw(e) {
+    this.mousePressed = true;
+    this.draw(e, true);
   }
 
   closeCurrPath() {
     this.mousePressed = false;
-    if ( this.penStyle === PenStyleEnum.Web ) {
+    if (this.penStyle === PenStyleEnum.Web) {
       this.points.length = 0;
     }
+  }
+
+  setPenStyle(penStyle) {
+    this.penStyle = penStyle;
+    this.points.length = 0;
+    this.lastX = null;
+    this.lastY = null;
   }
 
   setBgColor() {
@@ -76,33 +89,36 @@ export class SmartSlateComponent implements OnInit {
   draw(e, isPoint) {
     let currX, currY, oneFingerTouch;
     if (e instanceof TouchEvent) {
-      if ( e.targetTouches.length === 1 ) { // One finger
+      if (e.targetTouches.length === 1) { // One finger
         oneFingerTouch = e.targetTouches[0];
         currX = oneFingerTouch.pageX;
         currY = oneFingerTouch.pageY;
       }
-    }else if (e instanceof MouseEvent ) {
+    } else if (e instanceof MouseEvent) {
       currX = e.pageX;
       currY = e.pageY;
     }
 
-    if ( this.penStyle === PenStyleEnum.Web ) {
+    if (this.penStyle === PenStyleEnum.Web) {
       this.points.push({x: currX, y: currY});
     }
 
     this.ctx.beginPath();
     if (isPoint) {
       this.ctx.arc(currX, currY, this.penSize / 2, 0, 2 * Math.PI);
-      this.ctx.fillStyle = this.penColor;
-      this.ctx.fill();
+      if (this.penStyle !== PenStyleEnum.Pattern) {
+        this.ctx.fillStyle = this.penColor;
+        this.ctx.fill();
+      }
     } else {
       switch (this.penStyle) {
-        case PenStyleEnum.Basic:
+        case PenStyleEnum.Basic:    //  Basic
+        case PenStyleEnum.Pattern:  //  Pattern
           this.ctx.moveTo(this.lastX, this.lastY);
           this.ctx.lineTo(currX, currY);
           this.ctx.closePath();
           break;
-        case PenStyleEnum.Web:
+        case PenStyleEnum.Web:      //  Web
           this.ctx.moveTo(this.points[0].x, this.points[0].y);
           for (let i = 1; i < this.points.length; i++) {
             this.ctx.lineTo(this.points[i].x, this.points[i].y);
@@ -118,7 +134,7 @@ export class SmartSlateComponent implements OnInit {
 
       this.ctx.lineJoin = 'round';
       this.ctx.lineWidth = this.penSize;
-      this.ctx.strokeStyle = this.penColor;
+      this.ctx.strokeStyle = this.penStyle === PenStyleEnum.Pattern ? this.getPattern() : this.penColor;
       this.ctx.stroke();
     }
 
@@ -130,4 +146,22 @@ export class SmartSlateComponent implements OnInit {
     this.ctx.clearRect(0, 0, this.width, this.height);
     this.points.length = 0;
   }
+
+  getPattern() {
+    const patternCanvas = document.createElement('canvas'),
+      dotWidth = 20,
+      dotDistance = 5,
+      ctx = patternCanvas.getContext('2d');
+
+    patternCanvas.width = patternCanvas.height = 10;
+    ctx.strokeStyle = 'green';
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.moveTo(0, 5);
+    ctx.lineTo(10, 5);
+    ctx.closePath();
+    ctx.stroke();
+    return ctx.createPattern(patternCanvas, 'repeat');
+  }
+
 }
